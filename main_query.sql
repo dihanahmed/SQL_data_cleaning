dc21b5388394288aafee578f6d0fda6a6cb20635
dihanahmed <dihanahmed205@gmail.com>
2025-09-08 00:15:20 +1000

    removing some unnecessary columns

--- main_query.sql
+++ main_query.sql
@@ -198,20 +198,20 @@ where company like "Bally%";
 
 -- (4)deleting some unnecessary columns -----------------------------------------------------
 
--- select * 
--- from layoffs_staging2
--- where total_laid_off  is null
--- and percentage_laid_off is null;
+select * 
+from layoffs_staging2
+where total_laid_off  is null
+and percentage_laid_off is null;
 
 
--- delete
--- from layoffs_staging2
--- where total_laid_off  is null
--- and percentage_laid_off is null;
+delete
+from layoffs_staging2
+where total_laid_off  is null
+and percentage_laid_off is null;
 
 
--- select *
--- from layoffs_staging2;
+select *
+from layoffs_staging2;
 
--- alter table layoffs_staging2
--- drop column row_num;
+alter table layoffs_staging2
+drop column row_num;
