0e6f357454b275d13aa435738860956074e893df
dihanahmed <dihanahmed205@gmail.com>
2025-09-09 18:42:23 +1000

    checking layoffs

--- EDA.sql
+++ EDA.sql
@@ -1,2 +1,16 @@
 select *
 from layoffs_staging2;
+
+select max(total_laid_off), max(percentage_laid_off)
+from layoffs_staging2;
+
+select *
+from layoffs_staging2
+where percentage_laid_off=1
+order by funds_raised_millions desc;
+
+select company, sum(total_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+
